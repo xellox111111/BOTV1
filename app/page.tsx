1a9a3c1a8d6fb9c53b9772723060f2bea8f0fb91
Truong <truong77kk@gmail.com>
2026-06-01 11:30:21 +0700

    Upload source

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <iframe
+      src="/qua-tang-yeu-thuong.html"
+      title="Quà Tặng Yêu Thương"
+      style={{ position: "fixed", inset: 0, width: "100%", height: "100%", border: "none" }}
+    />
+  )
+}
